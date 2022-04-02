6e3c4e3d441e289dcf71bb7d73a4e91329800551
JohnBardoe <jbardoe@yandex.ru>
2022-04-02 18:19:14 +0400

    Fix pipeline

--- start.sh
+++ start.sh
@@ -2,18 +2,22 @@
 
 > /tmp/bot.log # clear log (or create it)
 
-python3 -u bot.py > /tmp/bot.log &
+python3 -u bot.py &> /tmp/bot.log & disown
 
+echo "Entering loop"
 # wait for word Started in log and check if bot is running
 while true; do
     if grep -q "Started" /tmp/bot.log; then
 	echo "Bot started"
 	break
     fi
+
     #check if bot is running
-    if ! ps -ef | grep -v grep | grep -q "bot.py"; then
-	echo /tmp/bot.log
+    if ! ps -p $! > /dev/null; then
+	echo "Bot died"
+	cat /tmp/bot.log
 	exit 1
     fi
-    sleep 1
+
+   sleep 1
 done
