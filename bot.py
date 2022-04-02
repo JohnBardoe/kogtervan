b4cf49acc3b0b97dfd60ab470fce3a8c93fdc30c
JohnBardoe <jbardoe@yandex.ru>
2022-04-02 18:38:31 +0400

    Created 2 buttons

--- bot.py
+++ bot.py
@@ -5,30 +5,59 @@ import os
 
 
 updater = Updater(os.environ.get("TELEGRAM_TOKEN"), use_context=True)
-#mongo_client = MongoClient("mongodb://localhost:1651/")
+# mongo_client = MongoClient("mongodb://localhost:1651/")
 
-#create start callback
+# create
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привет {update.effective_user.first_name}! Я бот для поиска людей для хобби работы.')
+    update.message.reply_text(
+        f"Привет {update.effective_user.first_name}! Ты тут зачем?",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                InlineKeyboardButton("Зарегистрируй меня!", callback_data="1"),
+                InlineKeyboardButton("Я бы нашел кого себе...", callback_data="2"),
+            ]
+        ),
+    )
+
+
+def button(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text=f"Selected option: {query.data}")
+
+# create callback for registration
+def select_city(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(
+        "Выбери город:",
+        reply_markup=ReplyKeyboardMarkup(["Москва", "Санкт-Петербург", "Новосибирск"]),
+    )
+    return select_city_2
+
+
+def registerHandlers():
+    print("Registering handlers...")
+    # create dispatcher
+    dp = updater.dispatcher
+    # create 2 stage conversation handler
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            select_city: [MessageHandler(Filters.text, select_city)],
+            select_city_2: [MessageHandler(Filters.text, select_city_2)],
+        },
+        fallbacks=[CommandHandler("start", start)],
+    )
+
+    # register handler
+    dp.add_handler(conv_handler)
+    dp.add_handler(CallbackQueryHandler(button))
 
 def main():
-    print("Registering handlers...")
-    start_handler = CommandHandler('start', start)
-    updater.dispatcher.add_handler(start_handler)
+    registerHandlers()
     updater.start_polling()
     print("Started")
     updater.idle()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
