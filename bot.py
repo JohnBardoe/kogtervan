35aedcddbd1ccf48dc5c13433ef4b6222640cced
JohnBardoe <jbardoe@yandex.ru>
2022-04-04 17:44:15 +0400

    Fix paths

--- bot.py
+++ bot.py
@@ -393,9 +393,10 @@ def registerHandlers():
             REGISTER: [register_handler],
             SEARCH: [search_handler]
         },
+        allow_reentry=True,
         fallbacks=[CommandHandler("cancel", cancel)]
     )
-
+    
     # register handler
     dp.add_handler(conv_handler)
     dp.add_error_handler(error_handler)
