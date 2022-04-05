676e2ceef84d1cbc3bba3bc11bd4137cb50054ba
JohnBardoe <jbardoe@yandex.ru>
2022-04-05 04:46:10 +0400

    Fix city

--- bot.py
+++ bot.py
@@ -119,15 +119,18 @@ def start(update: Update, context: CallbackContext) -> str:
 
 
 def select_city(update: Update, context: CallbackContext) -> str:
-    city = update.effective_message.text
+    update.callback_query.answer()
+    query = update.callback_query
+    city = query.data
     # get user_data context
-    user_data = context.user_data
-    # set new context user_input
-    user_data['user_input'] = city
-    context.user_data['user_input'] = user_data['user_input']
+    if not query.data:
+        city = update.effective_message.text
+        user_data = context.user_data
+        # set new context user_input
+        user_data['user_input'] = city
+        context.user_data['user_input'] = user_data['user_input']
     
     user_id = update.effective_user.id
-    # full text search in collection cities
 
     # find close matches to input city
     close_matches = difflib.get_close_matches(
@@ -394,7 +397,7 @@ def registerHandlers():
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
-            CITY_SELECT: [MessageHandler(Filters.text, select_city)],
+            CITY_SELECT: [CallbackQueryHandler(select_city)],
             ASK_PURPOSE: [CallbackQueryHandler(ask_purpose)],
             REGISTER: [register_handler],
             SEARCH: [search_handler]
